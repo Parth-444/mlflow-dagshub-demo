4b005be47f51a34b0681140870360a348ce17e20
Parth-444 <parthkannu0@gmail.com>
2025-12-10 20:06:23 +0530

    add dagshub config code

---
+++ iris-dt.py
@@ -0,0 +1,56 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import mlflow
+
+import dagshub
+dagshub.init(repo_owner='Parth-444', repo_name='mlflow-dagshub-demo', mlflow=True)
+
+# point MLflow to a local mlruns folder (use an absolute path)
+mlflow.set_tracking_uri("https://dagshub.com/Parth-444/mlflow-dagshub-demo.mlflow")
+
+# 1. Load the iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# 2. Split into train & test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+max_depth = 15
+# 3. Create and train the Random Forest model
+
+mlflow.set_experiment("iris-dt")
+with mlflow.start_run(): 
+
+    dt = DecisionTreeClassifier(max_depth=max_depth)
+
+    dt.fit(X_train, y_train)
+
+    # 4. Predict on test data
+    y_pred = dt.predict(X_test)
+
+    # 5. Calculate accuracy
+    accuracy = accuracy_score(y_test, y_pred)
+
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_param("max_depth", max_depth)
+
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6, 6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=iris.target_names, yticklabels=iris.target_names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('Confusion Matrix')
+    plt.savefig('confusion_matrix.png')
+    mlflow.log_artifact('confusion_matrix.png')
+    mlflow.log_artifact(__file__)
+    mlflow.sklearn.log_model(dt, 'decision tree')
+    mlflow.set_tag("model", "decision tree")
+    mlflow.set_tag('author', 'personal')
+    print('accuracy', accuracy)
